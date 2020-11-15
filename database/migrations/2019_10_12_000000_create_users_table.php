387266f70aa339fc9829ee22db39d1e19543ac88
Dela Akakpo <delprane14@gmail.com>
2020-11-15 11:01:33 +0000

    adds the aprroved column to user's table

--- database/migrations/2019_10_12_000000_create_users_table.php
+++ database/migrations/2019_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('lastname');
             $table->string('firstname');
+            $table->boolean('approved')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
